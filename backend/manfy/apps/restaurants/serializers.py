d63e670d274f290c5341d826b20dbf9d39da55a1
cjuan-code <cjuaniestacio@gmail.com>
2022-02-02 19:27:46 +0100

    added getTablesByRestaurant and getFreeTablesByRestaurant

--- backend/manfy/apps/restaurants/serializers.py
+++ backend/manfy/apps/restaurants/serializers.py
@@ -1,35 +1,51 @@
 from pyexpat import model
 from rest_framework import serializers
 from .models import Restaurant,Table,Img
+import json
+from django.core.serializers import serialize
 
 class imgSerializer(serializers.ModelSerializer):
     class Meta:
         model = Img
         fields = ('id','url')
 
-class tableSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Table
-        fields = ('id','capacity','sector','id_restaurant_id')
-
-    def to_img(self,instance) :
-        return {
-            'id': instance.id,
-            'url': instance.url
-        }
-
-class tableSerializer(serializers.ModelSerializer):
+class TableSerializer(serializers.ModelSerializer):
     class Meta:
         model = Table
         fields = ('id','capacity','sector','restaurant_id')
 
-        def to_tables(self,instance):
-            return{
-                'id': instance.id,
-                'capacity': instance.capacity,
-                'sector': instance.sector,
-                'restaurant_id': instance.restaurant_id
-            }
+    def to_tables(instance):
+        return {
+            'id': instance.id,
+            'capacity': instance.capacity,
+            'sector': instance.sector,
+            'restaurant_id': instance.restaurant_id
+        }
+    
+    def getTablesByRestaurant(context):
+
+        queryset = Table.objects.filter(restaurant_id = context['restaurant_id'])
+
+        serialized_tables = []
+
+        for table in queryset.iterator():
+            ser_table = TableSerializer.to_tables(table)
+            serialized_tables.append(ser_table)
+
+        return serialized_tables
+
+    def GetFreeTablesByRestaurant(context):
+
+        queryset = Table.objects.raw("SELECT * FROM restaurants_table WHERE id NOT IN (SELECT r.table_id FROM reservations_reservation r WHERE r.hour = '" + context['hour'] + "' AND r.day ='" + context['day'] + "') AND restaurant_id = " + context['restaurant_id'])
+            
+        serialized_tables = []
+
+        for table in queryset.iterator():
+            ser_table = TableSerializer.to_tables(table)
+            serialized_tables.append(ser_table)
+
+        return serialized_tables
+
 
 class restaurantSerializer(serializers.ModelSerializer):
     class Meta:
--- backend/manfy/apps/restaurants/urls.py
+++ backend/manfy/apps/restaurants/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .view import (GetRestaurants,GetImages)
+from .view import TableView
 
 urlpatterns = [
     path('restaurants', GetRestaurants.as_view()),
-    path('restaurants/img',GetImages.as_view())
+    path('restaurants/img',GetImages.as_view()),
+    path('tables/gettablesbyrestaurant', TableView.as_view({'post': 'getTablesByRestaurant'})),
+    path('tables/getfreetablesbyrestaurant', TableView.as_view({'post': 'GetFreeTablesByRestaurant'}))
 ]
