6ae6df96414b6c96c7b70dd2065002282a3ff413
AdriRameti <adri7agu@gmail.com>
2022-02-04 16:27:27 +0100

    Update docker improvements, alert cookies, css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,11 @@ import './App.css';
 import HomeList from './pages/Home/home';
 import Navbar from './pages/Navbar/Navbar'
 import Footer from './pages/Footer/Footer'
+import Cookies from './components/Cookies/AlertCookies'
 function App() {
   return (
     <div className="App">
+        <Cookies/>
         <Navbar/>
         <HomeList/>
         <Footer/>
